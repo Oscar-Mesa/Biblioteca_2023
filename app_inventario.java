f06997479a31927cf7a3ca47856e87e65a1c6c3a
Oscar-Mesa <100879612+Oscar-Mesa@users.noreply.github.com>
2023-10-02 09:25:44 -0500

    agrego componentes
    creo el menú principal, menú de libros y la opción registrar libros

---
+++ app_inventario.java
@@ -0,0 +1,71 @@
+import javax.swing.JOptionPane;
+
+public class app_inventario {
+    static cls_inventario [] inventario = new cls_inventario[5];
+
+    static int int_posiciones = 0;
+
+    public static void main(String[] args){
+        fnt_menu_principal();
+    }
+
+
+    private static void fnt_registrar_libros(){
+
+        String isba = JOptionPane.showInputDialog(null,"ISBA");
+        String nombre = JOptionPane.showInputDialog(null,"Nombre");
+        String autores = JOptionPane.showInputDialog(null,"Autores");
+        int stock = Integer.parseInt(JOptionPane.showInputDialog(null,"Stock"));  
+        String fecha_p = JOptionPane.showInputDialog(null,"Fecha publicación");
+        String resumen = JOptionPane.showInputDialog(null,"Resumen");
+        String ubicacion = JOptionPane.showInputDialog(null,"Ubicación");
+        float valor = Float.parseFloat(JOptionPane.showInputDialog(null,"Valor"));
+
+        inventario[int_posiciones] = new cls_inventario(isba, nombre, autores, stock, fecha_p, resumen, ubicacion, valor);
+        int_posiciones += 1;
+        JOptionPane.showMessageDialog(null, "Registrado con éxito",
+        "Registrar",JOptionPane.INFORMATION_MESSAGE);
+    }
+
+
+//Aréa de menúes
+
+    private static void fnt_menu_principal(){
+        while(true){
+            int menu = Integer.parseInt(JOptionPane.showInputDialog(null,
+            "<<< MENU PRINCIPAL >>>\n" +
+            "1. Menú libros\n"+
+            "2. Menú compras\n"+
+            "3. Salir" ));
+        
+            if (menu == 1){
+                fnt_menu_libros();
+            }
+        }
+
+
+    }
+
+    private static void fnt_menu_libros(){
+        while(true){
+            int menu = Integer.parseInt(JOptionPane.showInputDialog(null,
+            "<<< MENU LIBROS >>>\n" +
+            "1. Registrar libros\n"+
+            "2. Actualizar\n"+
+            "3. Consultar\n"+
+            "4. Salir" ));
+        
+            if (menu == 1){
+                fnt_registrar_libros();
+            }
+            
+            if(menu == 2){
+
+            }
+        }
+    }
+
+
+}
+
+
---
+++ cls_inventario.java
@@ -0,0 +1,41 @@
+
+public class cls_inventario{
+    private String str_isba;
+    private String str_nombre;
+    private String str_autores;
+    private int int_stock;
+    private String str_fecha_p;
+    private String str_resumen;
+    private String str_ubicacion;
+    private float flt_valor;
+
+    public cls_inventario(String isba, String nombre, String autores, int stock, String fecha_p, String resumen,
+    String ubicacion, float valor){
+
+        this.str_isba = isba;
+        this.str_nombre = nombre;
+        this.str_autores = autores;
+        this.int_stock = stock;
+        this.str_fecha_p = fecha_p;
+        this.str_resumen = resumen;
+        this.str_ubicacion = ubicacion;
+        this.flt_valor = valor;
+    }
+
+    public void setNombre(String nombre){this.str_nombre = nombre;}
+    public void setAutores(String autores){this.str_autores = autores;}
+    public void setFecha_p(String fecha_p){this.str_fecha_p = fecha_p;}
+    public void setResumen(String resumen){this.str_resumen = resumen;}
+    public void setUbicacion(String ubicacion){this.str_ubicacion = ubicacion;}
+
+
+    public String getIsba(){return this.str_isba;}
+    public String getNombre(){return this.str_nombre;}
+    public String getAutores(){return this.str_autores;}
+    public int getStock(){return this.int_stock;}
+    public String getFecha_P(){return this.str_fecha_p;}
+    public String getResumen(){return this.str_resumen;}
+    public String getUbicacion(){return this.str_ubicacion;}
+    public float getValor(){return this.flt_valor;}
+
+}
